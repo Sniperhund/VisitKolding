bfa8f672dc1847908a60d05898a1da8c9a41ee8d
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-04-29 09:52:26 +0200

    Færdiggjorde "At bo i Kolding"

--- pages/at-bo-i-kolding.vue
+++ pages/at-bo-i-kolding.vue
@@ -1,5 +1,75 @@
 <template>
 	<Header imgUrl="/kolding.jpg" title="At bo i Kolding" />
 
+	<section class="image-text-mix limit-width">
+		<article>
+			<div>
+				<h3>Boligmuligheder i Kolding</h3>
+				<p>
+					Når det kommer til boligmuligheder i Kolding, er der en bred
+					vifte af valgmuligheder til rådighed for enhver smag og
+					budget. Byen tilbyder alt fra hyggelige lejligheder i
+					centrum til rummelige familiehuse i mere rolige kvarterer.
+					Uanset om du foretrækker det urbane liv i midten af byen
+					eller den idylliske ro i udkanten, er der muligheder for
+					alle. Kolding har også et aktivt boligmarked, der konstant
+					udvikler sig for at imødekomme beboernes behov og ønsker.
+				</p>
+			</div>
+			<img src="/lejligheder.jpg" />
+		</article>
+		<article>
+			<img src="/studie.jpg" />
+			<div>
+				<h3>Studiemuligheder i Kolding</h3>
+				<p>
+					Kolding er hjemsted for flere uddannelsesinstitutioner, der
+					tilbyder et bredt udvalg af uddannelser inden for
+					forskellige fagområder. Fra gymnasier til erhvervsskoler og
+					videregående uddannelser, er der noget for enhver smag.
+					Uanset om du er interesseret i humaniora, naturvidenskab,
+					eller erhvervsøkonomi, finder du et passende studiemiljø i
+					Kolding.
+				</p>
+			</div>
+		</article>
+		<article>
+			<div>
+				<h3>Fritidsaktiviteter i Kolding</h3>
+				<p>
+					Kolding byder på et væld af fritidsaktiviteter, der kan
+					tilfredsstille enhver interesse. Uanset om du er til sport,
+					kultur, eller natur, er der masser af muligheder for at
+					udforske og deltage i spændende aktiviteter. Fra
+					sportsklubber til kulturelle arrangementer og naturskønne
+					områder, er der noget for enhver smag i Kolding.
+				</p>
+			</div>
+			<img src="/sport.jpg" />
+		</article>
+		<article>
+			<img src="/indkøb.jpg" />
+			<div>
+				<h3>Indkøbsmuligheder i Kolding</h3>
+				<p>
+					Kolding byder på et bredt udvalg af indkøbsmuligheder, der
+					imødekommer alles behov og ønsker. Fra store supermarkeder
+					og shoppingcentre til hyggelige lokale butikker og
+					specialforretninger, er der rig mulighed for at shoppe løs
+					og finde alt, hvad hjertet begærer. Derudover afholdes der
+					også regelmæssigt markeder og loppemarkeder, hvor du kan
+					gøre en god handel og opleve den lokale atmosfære.
+				</p>
+			</div>
+		</article>
+	</section>
+
 	<Footer />
 </template>
+
+<style scoped>
+h3 {
+	font-size: 2rem;
+	font-weight: 500;
+}
+</style>
